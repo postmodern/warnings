93bd44d903ed0ccfbd55bac31899075f7acfd865
Postmodern <postmodern.mod3@gmail.com>
2011-06-24 18:13:18 -0700

    Moved the at_exit hook into warnings/warnings.rb.

--- lib/warnings.rb
+++ lib/warnings.rb
@@ -3,21 +3,3 @@ require 'warnings/warnings'
 require 'warnings/mixin'
 
 include Warnings::Mixin
-
-at_exit do
-  if (!$WARNINGS.empty? && ($VERBOSE || $DEBUG))
-    $stderr.puts
-    $stderr.puts "Warnings:"
-    $stderr.puts
-
-    unique_warnings = {}
-
-    $WARNINGS.each do |warning|
-      unique_warnings[warning.source_location] ||= warning
-    end
-
-    unique_warnings.each_value { |warning| warning.print }
-
-    $stderr.puts
-  end
-end
--- lib/warnings/warnings.rb
+++ lib/warnings/warnings.rb
@@ -2,6 +2,24 @@ module Warnings
   # The global list of warnings
   $WARNINGS ||= []
 
+  at_exit do
+    if (!$WARNINGS.empty? && ($VERBOSE || $DEBUG))
+      $stderr.puts
+      $stderr.puts 'Warnings:'
+      $stderr.puts
+
+      unique_warnings = {}
+
+      $WARNINGS.each do |warning|
+        unique_warnings[warning.source_location] ||= warning
+      end
+
+      unique_warnings.each_value { |warning| warning.print }
+
+      $stderr.puts
+    end
+  end
+
   #
   # Selects all warnings with a similar message.
   #
